566cb73f88ecd71cd3f1508a4f9784c34eaa94e1
Rizwan Choudrey <rizwan.choudrey@gmail.com>
2014-09-15 14:43:57 +0100

    test

--- Reviewer/src/com/chdryra/android/reviewer/ActivityFeed.java
+++ Reviewer/src/com/chdryra/android/reviewer/ActivityFeed.java
@@ -5,7 +5,7 @@ import android.app.Fragment;
 import com.chdryra.android.mygenerallibrary.ActivitySingleFragment;
 
 public class ActivityFeed extends ActivitySingleFragment {
-
+    //new branch test
 	@Override
 	protected Fragment createFragment() {
 		return new FragmentFeed();
