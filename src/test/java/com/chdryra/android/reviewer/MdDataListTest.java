c4a9403ab69444f7f820b3524e17eb51e2d455c4
Rizwan Choudrey <rizwan.choudrey@gmail.com>
2015-12-08 16:38:33 +0000

    Trying to get tests to commit

--- src/test/java/com/chdryra/android/reviewer/MdDataListTest.java
+++ src/test/java/com/chdryra/android/reviewer/MdDataListTest.java
@@ -1,9 +1,88 @@
 package com.chdryra.android.reviewer;
 
+import com.chdryra.android.reviewer.Model.Implementation.ReviewsModel.Implementation.MdDataList;
+import com.chdryra.android.reviewer.Model.Implementation.ReviewsModel.Implementation.MdReviewId;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Created by: Rizwan Choudrey
  * On: 08/12/2015
  * Email: rizwan.choudrey@gmail.com
  */
 public class MdDataListTest {
+
+    @Test
+    public void getReviewIdReturnsCorrectId() {
+        MdReviewId correctId = new MdReviewId("abc", 123l, 1);
+        MdDataList<MdReviewId> ids = new MdDataList<>(correctId);
+        assertThat(ids.getReviewId(), is(correctId.toString()));
+    }
+
+    @Test
+    public void testContainsIdReturnsTrueIfTrue() {
+        MdReviewId holderId = new MdReviewId("abc", 123l, 1);
+        MdDataList<MdReviewId> ids = new MdDataList<>(holderId);
+        MdReviewId idDatum = new MdReviewId("cde", 123l, 1);
+        ids.add(idDatum);
+        assertThat(ids.containsId(idDatum.getReviewId()), is(true));
+    }
+
+    @Test
+    public void testContainsIdReturnsFalseIfFalse() {
+        MdReviewId id = new MdReviewId("abc", 123l, 1);
+        MdDataList<MdReviewId> ids = new MdDataList<>(id);
+        MdReviewId idDatum1 = new MdReviewId("cde", 123l, 1);
+        MdReviewId idDatum2 = new MdReviewId("cde", 123l, 2);
+        ids.add(idDatum1);
+        assertThat(ids.containsId(idDatum2.getReviewId()), is(false));
+    }
+
+    @Test
+    public void testGetItemNotNullIfContains() {
+        MdReviewId holderId = new MdReviewId("abc", 123l, 1);
+        MdDataList<MdReviewId> ids = new MdDataList<>(holderId);
+        MdReviewId idDatum = new MdReviewId("cde", 123l, 1);
+        ids.add(idDatum);
+        assertThat(ids.getItem(idDatum.getReviewId()), is(idDatum));
+    }
+
+    @Test
+    public void testGetItemNullIfNotContains() {
+        MdReviewId id = new MdReviewId("abc", 123l, 1);
+        MdDataList<MdReviewId> ids = new MdDataList<>(id);
+        MdReviewId idDatum1 = new MdReviewId("cde", 123l, 1);
+        MdReviewId idDatum2 = new MdReviewId("cde", 123l, 2);
+        ids.add(idDatum1);
+        assertThat(ids.getItem(idDatum2.getReviewId()), is(nullValue()));
+    }
+
+    @Test
+    public void testRemoveIfContains() {
+        MdReviewId holderId = new MdReviewId("abc", 123l, 1);
+        MdDataList<MdReviewId> ids = new MdDataList<>(holderId);
+        MdReviewId idDatum = new MdReviewId("cde", 123l, 1);
+        ids.add(idDatum);
+        assertThat(ids.getItem(idDatum.getReviewId()), is(idDatum));
+
+        ids.remove(idDatum.getReviewId());
+        assertThat(ids.getItem(idDatum.getReviewId()), is(nullValue()));
+    }
+
+    @Test
+    public void testDoesNotRemoveIfNotContains() {
+        MdReviewId holderId = new MdReviewId("abc", 123l, 1);
+        MdDataList<MdReviewId> ids = new MdDataList<>(holderId);
+        MdReviewId idDatum1 = new MdReviewId("cde", 123l, 1);
+        MdReviewId idDatum2 = new MdReviewId("cde", 123l, 2);
+        ids.add(idDatum1);
+        assertThat(ids.getItem(idDatum1.getReviewId()), is(idDatum1));
+
+        ids.remove(idDatum2.getReviewId());
+        assertThat(ids.getItem(idDatum1.getReviewId()), is(idDatum1));
+    }
 }
