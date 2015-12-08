d3343c3fa317c0ced95c1209a42deee65194e284
Rizwan Choudrey <rizwan.choudrey@gmail.com>
2015-12-08 16:31:42 +0000

    trying to get tests to commit

---
+++ src/test/java/com/chdryra/android/reviewer/ReviewUserTest.java
@@ -0,0 +1,19 @@
+package com.chdryra.android.reviewer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Created by: Rizwan Choudrey
+ * On: 08/12/2015
+ * Email: rizwan.choudrey@gmail.com
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ReviewUserTest {
+
+    @Test
+    public void checkDataReviewIdsMatchPassedReviewId() {
+
+    }
+}
