c0467357f5ea2cb87f2b534b679cb1c0262c5a5c
Rizwan Choudrey <rizwan.choudrey@gmail.com>
2016-01-11 14:31:25 +0000

    Added comparator tests for Comments

--- src/test/java/test/DataAlgorithms/Sorting/AuthorAlphabeticalTest.java
+++ src/test/java/test/DataAlgorithms/Sorting/AuthorAlphabeticalTest.java
@@ -1,7 +1,6 @@
 package test.DataAlgorithms.Sorting;
 
-import com.chdryra.android.reviewer.DataAlgorithms.DataSorting.Factories.DataComparatorsFactory;
-import com.chdryra.android.reviewer.DataAlgorithms.DataSorting.Interfaces.ComparatorCollection;
+import com.chdryra.android.reviewer.DataAlgorithms.DataSorting.Implementation.AuthorAlphabetical;
 import com.chdryra.android.reviewer.DataDefinitions.Implementation.DatumAuthor;
 import com.chdryra.android.reviewer.DataDefinitions.Interfaces.DataAuthor;
 import com.chdryra.android.reviewer.Model.Implementation.UserModel.AuthorId;
@@ -9,42 +8,37 @@ import com.chdryra.android.testutils.RandomString;
 
 import org.junit.Test;
 
-import java.util.Comparator;
-
 /**
  * Created by: Rizwan Choudrey
  * On: 11/01/2016
  * Email: rizwan.choudrey@gmail.com
  */
-public class AuthorComparatorsTest extends ComparatorCollectionImplTest<DataAuthor>{
+public class AuthorAlphabeticalTest extends ComparatorTest<DataAuthor> {
 
-    @Override
-    protected ComparatorCollection<DataAuthor> getComparators(DataComparatorsFactory factory) {
-        return factory.getAuthorComparators();
+    public AuthorAlphabeticalTest() {
+        super(new AuthorAlphabetical());
     }
 
     @Test
-    public void defaultComparatorIsAlphabeticalAscendingDifferentFirstLetter() {
-        Comparator<DataAuthor> comparator = getDefaultComparator();
+    public void alphabeticalAscendingDifferentFirstLetter() {
         DataAuthor A = new DatumAuthor("A" + RandomString.nextWord(), AuthorId.generateId());
         DataAuthor B = new DatumAuthor("B" + RandomString.nextWord(), AuthorId.generateId());
         DataAuthor C = new DatumAuthor("C" + RandomString.nextWord(), AuthorId.generateId());
 
-        ComparatorTester<DataAuthor> tester = newComparatorTester(comparator);
+        ComparatorTester<DataAuthor> tester = newComparatorTester();
         tester.testFirstSecond(A, B);
         tester.testFirstSecond(B, C);
         tester.testFirstSecond(A, C);
     }
 
     @Test
-    public void defaultComparatorIsAlphabeticalAscendingStartingStemSame() {
-        Comparator<DataAuthor> comparator = getDefaultComparator();
+    public void alphabeticalAscendingStartingStemSame() {
         String name1 = RandomString.nextWord();
         String name2 = name1 + RandomString.nextWord();
         DataAuthor author1 = new DatumAuthor(name1, AuthorId.generateId());
         DataAuthor author2 = new DatumAuthor(name2, AuthorId.generateId());
 
-        ComparatorTester<DataAuthor> tester = newComparatorTester(comparator);
+        ComparatorTester<DataAuthor> tester = newComparatorTester();
         if(name1.compareToIgnoreCase(name2) < 0) {
             tester.testFirstSecond(author1, author2);
         } else if(name1.compareToIgnoreCase(name2) > 0){
@@ -55,26 +49,24 @@ public class AuthorComparatorsTest extends ComparatorCollectionImplTest<DataAuth
     }
 
     @Test
-    public void defaultComparatorEquality() {
-        Comparator<DataAuthor> comparator = getDefaultComparator();
+    public void comparatorEquality() {
         String name = RandomString.nextWord();
         DataAuthor author = new DatumAuthor(name, AuthorId.generateId());
         DataAuthor author2 = new DatumAuthor(name, AuthorId.generateId());
 
-        ComparatorTester<DataAuthor> tester = newComparatorTester(comparator);
+        ComparatorTester<DataAuthor> tester = newComparatorTester();
         tester.testEquals(author, author);
         tester.testEquals(author, author2);
     }
 
     @Test
-    public void defaultComparatorEqualityIgnoresCase() {
-        Comparator<DataAuthor> comparator = getDefaultComparator();
+    public void comparatorEqualityIgnoresCase() {
         String name = RandomString.nextWord();
         DataAuthor author = new DatumAuthor(name, AuthorId.generateId());
         DataAuthor authorLower = new DatumAuthor(name.toLowerCase(), AuthorId.generateId());
         DataAuthor authorUpper = new DatumAuthor(name.toUpperCase(), AuthorId.generateId());
 
-        ComparatorTester<DataAuthor> tester = newComparatorTester(comparator);
+        ComparatorTester<DataAuthor> tester = newComparatorTester();
         tester.testEquals(author, authorLower);
         tester.testEquals(author, authorUpper);
     }
--- src/test/java/test/DataAlgorithms/Sorting/CriterionSubjectThenRatingTest.java
+++ src/test/java/test/DataAlgorithms/Sorting/CriterionSubjectThenRatingTest.java
@@ -1,14 +1,11 @@
 package test.DataAlgorithms.Sorting;
 
-import com.chdryra.android.reviewer.DataAlgorithms.DataSorting.Factories.DataComparatorsFactory;
-import com.chdryra.android.reviewer.DataAlgorithms.DataSorting.Interfaces.ComparatorCollection;
+import com.chdryra.android.reviewer.DataAlgorithms.DataSorting.Implementation.CriterionSubjectThenRating;
 import com.chdryra.android.reviewer.DataDefinitions.Implementation.DatumCriterion;
 import com.chdryra.android.reviewer.DataDefinitions.Interfaces.DataCriterion;
 import com.chdryra.android.testutils.RandomString;
 
 import org.junit.Test;
-
-import java.util.Comparator;
 
 import test.TestUtils.RandomRating;
 import test.TestUtils.RandomReviewId;
@@ -18,24 +15,21 @@ import test.TestUtils.RandomReviewId;
  * On: 05/09/2015
  * Email: rizwan.choudrey@gmail.com
  */
-public class CriterionComparatorsTest extends ComparatorCollectionImplTest<DataCriterion> {
+public class CriterionSubjectThenRatingTest extends ComparatorTest<DataCriterion> {
 
-    @Override
-    protected ComparatorCollection<DataCriterion> getComparators(DataComparatorsFactory factory) {
-        return factory.getCriterionComparators();
+    public CriterionSubjectThenRatingTest() {
+        super(new CriterionSubjectThenRating());
     }
 
     @Test
-    public void defaultComparatorIsSubjectAlphabeticalAscendingFirst_DescendingRatingSecond() {
-        Comparator<DataCriterion> comparator = getDefaultComparator();
-
+    public void subjectAlphabeticalAscendingFirst_DescendingRatingSecond() {
         DataCriterion reviewA2 = new DatumCriterion(RandomReviewId.nextReviewId(), "A", 2f);
         DataCriterion reviewA1 = new DatumCriterion(RandomReviewId.nextReviewId(), "A", 1f);
         DataCriterion reviewb5 = new DatumCriterion(RandomReviewId.nextReviewId(), "b", 5f);
         DataCriterion reviewB2 = new DatumCriterion(RandomReviewId.nextReviewId(), "B", 2f);
         DataCriterion reviewc1 = new DatumCriterion(RandomReviewId.nextReviewId(), "c", 1f);
 
-        ComparatorTester<DataCriterion> tester = newComparatorTester(comparator);
+        ComparatorTester<DataCriterion> tester = newComparatorTester();
         tester.testFirstSecond(reviewA2, reviewA1);
         tester.testFirstSecond(reviewA1, reviewb5);
         tester.testFirstSecond(reviewb5, reviewB2);
@@ -43,45 +37,40 @@ public class CriterionComparatorsTest extends ComparatorCollectionImplTest<DataC
     }
 
     @Test
-    public void defaultComparatorIsSubjectAlphabeticalAscendingDifferentFirstLetter_SameRating() {
-        Comparator<DataCriterion> comparator = getDefaultComparator();
-
+    public void subjectAlphabeticalAscendingDifferentFirstLetter_SameRating() {
         float rating = RandomRating.nextRating();
         DataCriterion reviewA = new DatumCriterion(RandomReviewId.nextReviewId(), "A" + RandomString.nextWord(), rating);
         DataCriterion reviewB = new DatumCriterion(RandomReviewId.nextReviewId(), "B" + RandomString.nextWord(), rating);
         DataCriterion reviewC = new DatumCriterion(RandomReviewId.nextReviewId(), "C" + RandomString.nextWord(), rating);
 
-        ComparatorTester<DataCriterion> tester = newComparatorTester(comparator);
+        ComparatorTester<DataCriterion> tester = newComparatorTester();
         tester.testFirstSecond(reviewA, reviewB);
         tester.testFirstSecond(reviewB, reviewC);
         tester.testFirstSecond(reviewA, reviewC);
     }
 
     @Test
-    public void defaultComparatorIsRatingDescending_SameSubject() {
-        Comparator<DataCriterion> comparator = getDefaultComparator();
-
+    public void ratingDescending_SameSubject() {
         String subject = RandomString.nextWord();
         DataCriterion review1 = new DatumCriterion(RandomReviewId.nextReviewId(), subject, 1f);
         DataCriterion review2 = new DatumCriterion(RandomReviewId.nextReviewId(), subject, 2f);
         DataCriterion review3 = new DatumCriterion(RandomReviewId.nextReviewId(), subject, 3f);
 
-        ComparatorTester<DataCriterion> tester = newComparatorTester(comparator);
+        ComparatorTester<DataCriterion> tester = newComparatorTester();
         tester.testFirstSecond(review2, review1);
         tester.testFirstSecond(review3, review2);
         tester.testFirstSecond(review3, review1);
     }
 
     @Test
-    public void defaultComparatorIsAlphabeticalAscendingStartingSubjectStemSame_SameRating() {
-        Comparator<DataCriterion> comparator = getDefaultComparator();
+    public void subjectAlphabeticalAscendingStartingSubjectStemSame_SameRating() {
         String subject1 = RandomString.nextWord();
         String subject2 = subject1 + RandomString.nextWord();
         float rating = RandomRating.nextRating();
         DataCriterion review1 = new DatumCriterion(RandomReviewId.nextReviewId(), subject1, rating);
         DataCriterion review2 = new DatumCriterion(RandomReviewId.nextReviewId(), subject2, rating);
 
-        ComparatorTester<DataCriterion> tester = newComparatorTester(comparator);
+        ComparatorTester<DataCriterion> tester = newComparatorTester();
         if(subject1.compareToIgnoreCase(subject2) < 0) {
             tester.testFirstSecond(review1, review2);
         } else if(subject1.compareToIgnoreCase(subject2) > 0){
@@ -92,26 +81,26 @@ public class CriterionComparatorsTest extends ComparatorCollectionImplTest<DataC
     }
 
     @Test
-    public void defaultComparatorEqualitySameSubject_SameRating() {
+    public void comparatorEqualitySameSubject_SameRating() {
         String subject  = RandomString.nextWord();
         float rating = RandomRating.nextRating();
-        DataCriterion criteiron1 = new DatumCriterion(RandomReviewId.nextReviewId(), subject, rating);
+        DataCriterion criterion1 = new DatumCriterion(RandomReviewId.nextReviewId(), subject, rating);
         DataCriterion criterion2 = new DatumCriterion(RandomReviewId.nextReviewId(), subject, rating);
 
-        ComparatorTester<DataCriterion> tester = newComparatorTester(getDefaultComparator());
-        tester.testEquals(criteiron1, criteiron1);
-        tester.testEquals(criteiron1, criterion2);
+        ComparatorTester<DataCriterion> tester = newComparatorTester();
+        tester.testEquals(criterion1, criterion1);
+        tester.testEquals(criterion1, criterion2);
     }
 
     @Test
-    public void defaultComparatorEqualityIgnoresSubjectCase_SameRating() {
+    public void comparatorEqualityIgnoresSubjectCase_SameRating() {
         String subject  = RandomString.nextWord();
         float rating = RandomRating.nextRating();
         DataCriterion review = new DatumCriterion(RandomReviewId.nextReviewId(), subject, rating);
         DataCriterion reviewLower = new DatumCriterion(RandomReviewId.nextReviewId(), subject.toLowerCase(), rating);
         DataCriterion reviewUpper = new DatumCriterion(RandomReviewId.nextReviewId(), subject.toUpperCase(), rating);
 
-        ComparatorTester<DataCriterion> tester = newComparatorTester(getDefaultComparator());
+        ComparatorTester<DataCriterion> tester = newComparatorTester();
         tester.testEquals(review, reviewLower);
         tester.testEquals(review, reviewUpper);
     }
