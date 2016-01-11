1c4956b3d9c8a4c32342b8f00c0524f5912b994c
Rizwan Choudrey <rizwan.choudrey@gmail.com>
2016-01-11 15:15:46 +0000

    Added comparator tests for Comments

---
+++ src/test/java/test/DataAlgorithms/Sorting/DateMostRecentFirstTest.java
@@ -0,0 +1,51 @@
+package test.DataAlgorithms.Sorting;
+
+import com.chdryra.android.reviewer.DataAlgorithms.DataSorting.Implementation.DateMostRecentFirst;
+import com.chdryra.android.reviewer.DataDefinitions.Implementation.DatumDateReview;
+import com.chdryra.android.reviewer.DataDefinitions.Interfaces.DataDate;
+
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.Random;
+
+import test.TestUtils.RandomReviewId;
+
+/**
+ * Created by: Rizwan Choudrey
+ * On: 11/01/2016
+ * Email: rizwan.choudrey@gmail.com
+ */
+public class DateMostRecentFirstTest extends ComparatorTest<DataDate>{
+    private static final Random RAND = new Random();
+
+    public DateMostRecentFirstTest() {
+        super(new DateMostRecentFirst());
+    }
+
+    @Test
+    public void alphabeticalAscendingDifferentFirstLetter() {
+        long first = new Date().getTime();
+        long second = first + 100l;
+        long third = second + 100l;
+        DataDate date1 = new DatumDateReview(RandomReviewId.nextReviewId(), third);
+        DataDate date2 = new DatumDateReview(RandomReviewId.nextReviewId(), second);
+        DataDate date3 = new DatumDateReview(RandomReviewId.nextReviewId(), first);
+
+        ComparatorTester<DataDate> tester = newComparatorTester();
+        tester.testFirstSecond(date1, date2);
+        tester.testFirstSecond(date2, date3);
+        tester.testFirstSecond(date1, date3);
+    }
+
+    @Test
+    public void comparatorEquality() {
+        long time = new Date().getTime();
+        DataDate date1 = new DatumDateReview(RandomReviewId.nextReviewId(), time);
+        DataDate date2 = new DatumDateReview(RandomReviewId.nextReviewId(), time);
+
+        ComparatorTester<DataDate> tester = newComparatorTester();
+        tester.testEquals(date1, date1);
+        tester.testEquals(date1, date2);
+    }
+}
---
+++ src/test/java/test/DataAlgorithms/Sorting/FactLabelThenValueTest.java
@@ -0,0 +1,127 @@
+package test.DataAlgorithms.Sorting;
+
+import com.chdryra.android.reviewer.DataAlgorithms.DataSorting.Implementation.FactLabelThenValue;
+import com.chdryra.android.reviewer.DataDefinitions.Implementation.DatumFact;
+import com.chdryra.android.reviewer.DataDefinitions.Interfaces.DataFact;
+import com.chdryra.android.testutils.RandomString;
+
+import org.junit.Test;
+
+import test.TestUtils.RandomReviewId;
+
+/**
+ * Created by: Rizwan Choudrey
+ * On: 11/01/2016
+ * Email: rizwan.choudrey@gmail.com
+ */
+public class FactLabelThenValueTest extends ComparatorTest<DataFact>{
+    public FactLabelThenValueTest() {
+        super(new FactLabelThenValue());
+    }
+
+    @Test
+    public void testGetDefaultComparator() {
+        DataFact AA = new DatumFact(RandomReviewId.nextReviewId(), "A", "A");
+        DataFact AA2 = new DatumFact(RandomReviewId.nextReviewId(), "A", "A");
+        DataFact Aa = new DatumFact(RandomReviewId.nextReviewId(), "A", "a");
+        DataFact aA = new DatumFact(RandomReviewId.nextReviewId(), "a", "A");
+        DataFact aa = new DatumFact(RandomReviewId.nextReviewId(), "a", "a");
+        DataFact AZ = new DatumFact(RandomReviewId.nextReviewId(), "A", "Z");
+        DataFact BC = new DatumFact(RandomReviewId.nextReviewId(), "B", "C");
+        DataFact CA = new DatumFact(RandomReviewId.nextReviewId(), "C", "A");
+
+        ComparatorTester<DataFact> tester = newComparatorTester();
+        tester.testEquals(AA, AA);
+        tester.testEquals(AA, AA2);
+        tester.testEquals(AA, Aa);
+        tester.testEquals(AA, aA);
+        tester.testEquals(AA, aa);
+        tester.testFirstSecond(AA, AZ);
+        tester.testFirstSecond(AZ, BC);
+        tester.testFirstSecond(BC, CA);
+        tester.testFirstSecond(AA, CA);
+    }
+
+    @Test
+    public void labelAlphabeticalAscendingFirst_AscendingValueSecond() {
+        DataFact AA = new DatumFact(RandomReviewId.nextReviewId(), "A", "A");
+        DataFact AZ = new DatumFact(RandomReviewId.nextReviewId(), "A", "Z");
+        DataFact BC = new DatumFact(RandomReviewId.nextReviewId(), "B", "C");
+        DataFact CA = new DatumFact(RandomReviewId.nextReviewId(), "C", "A");
+
+        ComparatorTester<DataFact> tester = newComparatorTester();
+        tester.testFirstSecond(AA, AZ);
+        tester.testFirstSecond(AZ, BC);
+        tester.testFirstSecond(BC, CA);
+        tester.testFirstSecond(AA, CA);
+    }
+
+    @Test
+    public void labelAlphabeticalAscendingDifferentFirstLetter_SameValue() {
+        String value = RandomString.nextWord();
+        DataFact factA = new DatumFact(RandomReviewId.nextReviewId(), "A" + RandomString.nextWord(), value);
+        DataFact factB = new DatumFact(RandomReviewId.nextReviewId(), "B" + RandomString.nextWord(), value);
+        DataFact factC = new DatumFact(RandomReviewId.nextReviewId(), "C" + RandomString.nextWord(), value);
+
+        ComparatorTester<DataFact> tester = newComparatorTester();
+        tester.testFirstSecond(factA, factB);
+        tester.testFirstSecond(factB, factC);
+        tester.testFirstSecond(factA, factC);
+    }
+
+    @Test
+    public void valueAscending_SameLabel() {
+        String label = RandomString.nextWord();
+        DataFact fact1 = new DatumFact(RandomReviewId.nextReviewId(), label, "A");
+        DataFact fact2 = new DatumFact(RandomReviewId.nextReviewId(), label, "B");
+        DataFact fact3 = new DatumFact(RandomReviewId.nextReviewId(), label, "C");
+
+        ComparatorTester<DataFact> tester = newComparatorTester();
+        tester.testFirstSecond(fact1, fact2);
+        tester.testFirstSecond(fact2, fact3);
+        tester.testFirstSecond(fact1, fact3);
+    }
+
+    @Test
+    public void labelAlphabeticalAscendingStartingLabelStemSame_SameValue() {
+        String label1 = RandomString.nextWord();
+        String label2 = label1 + RandomString.nextWord();
+        String value = RandomString.nextWord();
+        DataFact fact1 = new DatumFact(RandomReviewId.nextReviewId(), label1, value);
+        DataFact fact2 = new DatumFact(RandomReviewId.nextReviewId(), label2, value);
+
+        ComparatorTester<DataFact> tester = newComparatorTester();
+        if(label1.compareToIgnoreCase(label2) < 0) {
+            tester.testFirstSecond(fact1, fact2);
+        } else if(label1.compareToIgnoreCase(label2) > 0){
+            tester.testFirstSecond(fact2, fact1);
+        } else {
+            tester.testEquals(fact1, fact2);
+        }
+    }
+
+    @Test
+    public void comparatorEqualitySameLabel_SameValue() {
+        String label = RandomString.nextWord();
+        String value = RandomString.nextWord();
+        DataFact fact1 = new DatumFact(RandomReviewId.nextReviewId(), label, value);
+        DataFact fact2 = new DatumFact(RandomReviewId.nextReviewId(), label, value);
+
+        ComparatorTester<DataFact> tester = newComparatorTester();
+        tester.testEquals(fact1, fact1);
+        tester.testEquals(fact1, fact2);
+    }
+
+    @Test
+    public void comparatorEqualityIgnoresLabelCase_SameValue() {
+        String label = RandomString.nextWord();
+        String value  = RandomString.nextWord();
+        DataFact fact = new DatumFact(RandomReviewId.nextReviewId(), label, value);
+        DataFact factLower = new DatumFact(RandomReviewId.nextReviewId(), label.toLowerCase(), value.toUpperCase());
+        DataFact factUpper = new DatumFact(RandomReviewId.nextReviewId(), label.toUpperCase(), value.toLowerCase());
+
+        ComparatorTester<DataFact> tester = newComparatorTester();
+        tester.testEquals(fact, factLower);
+        tester.testEquals(fact, factUpper);
+    }
+}
