22054a902f5eef0499f470c58e82200a79bbd6cf
Rizwan Choudrey <rizwan.choudrey@gmail.com>
2016-02-25 13:17:43 +0000

    Added Twitter Fabric SDK

--- src/main/java/com/chdryra/android/reviewer/Social/Implementation/PlatformTwitter.java
+++ src/main/java/com/chdryra/android/reviewer/Social/Implementation/PlatformTwitter.java
@@ -12,26 +12,29 @@ import android.content.Context;
 
 import com.chdryra.android.reviewer.R;
 import com.chdryra.android.reviewer.Social.Interfaces.OAuthRequester;
+import com.crashlytics.android.Crashlytics;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 
 import io.fabric.sdk.android.Fabric;
+import twitter4j.auth.AccessToken;
 
 /**
  * Created by: Rizwan Choudrey
  * On: 23/02/2016
  * Email: rizwan.choudrey@gmail.com
  */
-public class PlatformTwitter extends SocialPlatformImpl<twitter4j.auth.AccessToken>{
+public class PlatformTwitter extends SocialPlatformImpl<AccessToken>{
     public static final String NAME = "twitter";
     public static final int KEY = R.string.consumer_key_twitter;
     public static final int SECRET = R.string.consumer_secret_twitter;
 
     public PlatformTwitter(Context context, PublisherTwitter publisher,
-                           OAuthRequester<twitter4j.auth.AccessToken> authRequester) {
+                           OAuthRequester<AccessToken> authRequester) {
         super(publisher, authRequester);
         TwitterAuthConfig authConfig
                 = new TwitterAuthConfig(context.getString(KEY), context.getString(SECRET));
         Fabric.with(context, new Twitter(authConfig));
+        Fabric.with(context, new Crashlytics());
     }
 }
