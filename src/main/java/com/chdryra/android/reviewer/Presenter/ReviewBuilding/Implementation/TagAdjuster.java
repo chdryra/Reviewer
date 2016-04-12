e67774f8c25a205a7556c724bcd7096df6c09eae
Rizwan Choudrey <rizwan.choudrey@gmail.com>
2016-04-12 14:39:17 +0100

    Fixed tag bug wheresubject tag was getting deleted when delete all was being used.

--- src/main/java/com/chdryra/android/reviewer/Presenter/ReviewBuilding/Implementation/TagAdjuster.java
+++ src/main/java/com/chdryra/android/reviewer/Presenter/ReviewBuilding/Implementation/TagAdjuster.java
@@ -24,6 +24,7 @@ public class TagAdjuster {
     }
 
     public void setCurrentSubjectTag(String tag) {
+        mCurrentSubjectTag = null;
         if(tag != null && tag.length() > 0) {
             mCurrentSubjectTag = new GvTag(tag);
         }
