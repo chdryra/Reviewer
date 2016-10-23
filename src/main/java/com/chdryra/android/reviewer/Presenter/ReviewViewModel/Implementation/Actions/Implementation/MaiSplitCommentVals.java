da18b7787aec9712bd6bf17c6b0fba283316280d
Rizwan Choudrey <rizwan.choudrey@gmail.com>
2016-10-23 18:23:22 +0100

    Some code cleanup

--- src/main/java/com/chdryra/android/reviewer/Presenter/ReviewViewModel/Implementation/Actions/Implementation/MaiSplitCommentVals.java
+++ src/main/java/com/chdryra/android/reviewer/Presenter/ReviewViewModel/Implementation/Actions/Implementation/MaiSplitCommentVals.java
@@ -6,7 +6,8 @@
  *
  */
 
-package com.chdryra.android.reviewer.Presenter.ReviewViewModel.Implementation.Actions.Implementation;
+package com.chdryra.android.reviewer.Presenter.ReviewViewModel.Implementation.Actions
+        .Implementation;
 
 
 import com.chdryra.android.reviewer.Presenter.ReviewBuilding.Implementation.ReviewCommentsEditor;
@@ -20,18 +21,22 @@ import com.chdryra.android.reviewer.Presenter.ReviewViewModel.Implementation.Dat
 public class MaiSplitCommentVals extends MaiSplitCommentsBasic<GvComment> {
     private boolean mSplit = false;
 
+    public void updateGridData() {
+        if (!isAttached() || getReviewView() == null) return;
+        setSplit();
+    }
+
     @Override
     protected void doSplit(boolean doSplit) {
         mSplit = doSplit;
+        setSplit();
+    }
+
+    private void setSplit() {
         try {
             ((ReviewCommentsEditor) getReviewView()).setSplit(mSplit);
         } catch (ClassCastException e) {
 
         }
-    }
-
-    public void updateGridData() {
-        if(!isAttached() || getReviewView() == null) return;
-        doSplit(mSplit);
     }
 }
